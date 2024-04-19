be413f8f605443e3d00a13587e6b79cb8078c5df
Abdullahi Abdulrofiu <abdulrofiuabdullahi@gmail.com>
2024-04-19 01:22:37 +0100

    update imported file

--- models/state.py
+++ models/state.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import environ
 import models
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
